86558f262570d14b87686af01348e0a48bc37070
ValfridoNovais <146680175+ValfridoNovais@users.noreply.github.com>
2024-07-12 03:20:14 -0300

    Criando estrutura do projeto

---
+++ src/site_flet.py
@@ -0,0 +1,8 @@
+# Titulo: MMPG_Novais
+# Botão: Iniciar Chat
+    # popup/modal/alerta
+        #Título: Bem Vindo ao MMPG_Zap
+        # Campo de Texto: Escreva seu nome no chat
+        # Botão: Entrar no chat
+            #Sumir com o Titulo e o Botão Inicial
+            #Fechar o poopup
