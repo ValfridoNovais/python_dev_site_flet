64722cf52f02e1ea1d99fda31cab34c14c593a40
ValfridoNovais <146680175+ValfridoNovais@users.noreply.github.com>
2024-07-12 03:47:54 -0300

    Criaçao das primeiras linhas do codigo

--- src/site_flet.py
+++ src/site_flet.py
@@ -6,3 +6,8 @@
         # Botão: Entrar no chat
             #Sumir com o Titulo e o Botão Inicial
             #Fechar o poopup
+            #Criar Chat (com a mensagem: Usucari entrou)
+
+# Flet -> aplicativo/site/programa de computador
+
+ex
